20d6b584255f537615873ad35b44446ed2c180e3
Veer0x1 <balveer.singhrao.eee21@itbhu.ac.in>
2023-05-27 10:24:07 +0530

    loading state for dashboard button

--- app/dashboard/components/DashboardButton.tsx
+++ app/dashboard/components/DashboardButton.tsx
@@ -1,24 +1,33 @@
 "use client"
 
 import Link from "next/link"
+import { Loader2 } from "lucide-react"
 import { useSession } from "next-auth/react"
 
 import { Button, buttonVariants } from "@/components/ui/button"
 
 export default function DashboardButton() {
-  const { data: session } = useSession()
-  return (
-    <div>
-      {session?.user ? (
-        <Button>
-          <Link
-            href={"/dashboard"}
-            
-          >
-            Dashboard
-          </Link>
+  const { data: session, status } = useSession()
+  if (status === "loading") {
+    return (
+      <div>
+        <Button disabled>
+          <Loader2 className="mr-2 h-4 w-4 animate-spin" />
+          Please wait
         </Button>
-      ) : null}
-    </div>
-  )
+      </div>
+    )
+  } else if (status === "authenticated") {
+    return (
+      <div>
+        {session?.user ? (
+          <Button>
+            <Link href={"/dashboard"}>Dashboard</Link>
+          </Button>
+        ) : null}
+      </div>
+    )
+  } else {
+    return null
+  }
 }
--- components/site-header.tsx
+++ components/site-header.tsx
@@ -5,11 +5,10 @@ import { Button, buttonVariants } from "@/components/ui/button"
 import { Icons } from "@/components/icons"
 import { MainNav } from "@/components/main-nav"
 import { ThemeToggle } from "@/components/theme-toggle"
-import SignupButton from "@/app/signup/components/SignupButton"
-import LoginButton from '../app/login/components/LoginButton';
 import DashboardButton from "@/app/dashboard/components/DashboardButton"
+import SignupButton from "@/app/signup/components/SignupButton"
 
-
+import LoginButton from "../app/login/components/LoginButton"
 
 export function SiteHeader() {
   return (
@@ -19,39 +18,8 @@ export function SiteHeader() {
         <div className="flex flex-1 items-center justify-end space-x-4">
           <nav className="flex items-center space-x-1">
             <DashboardButton />
-            <LoginButton/>
-            <SignupButton/>
-
-            <Link
-              href={siteConfig.links.github}
-              target="_blank"
-              rel="noreferrer"
-            >
-              <div
-                className={buttonVariants({
-                  size: "sm",
-                  variant: "ghost",
-                })}
-              >
-                <Icons.gitHub className="h-5 w-5" />
-                <span className="sr-only">GitHub</span>
-              </div>
-            </Link>
-            <Link
-              href={siteConfig.links.twitter}
-              target="_blank"
-              rel="noreferrer"
-            >
-              <div
-                className={buttonVariants({
-                  size: "sm",
-                  variant: "ghost",
-                })}
-              >
-                <Icons.twitter className="h-5 w-5 fill-current" />
-                <span className="sr-only">Twitter</span>
-              </div>
-            </Link>
+            <LoginButton />
+            <SignupButton />
             <ThemeToggle />
           </nav>
         </div>
